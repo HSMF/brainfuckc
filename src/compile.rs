ae7a079f4586e001e9467a73210359b9f92c1acd
Conradin Laux <conradin.laux@gmail.com>
2024-04-18 10:57:49 +0200

    chore: compile stub

---
+++ src/compile.rs
@@ -0,0 +1,6 @@
+use crate::{ast::Action, llvm};
+
+pub fn compile(code: &[Action]) -> llvm::Program {
+
+    todo!()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
+use compile::compile;
+
 use crate::ast::Ctx;
 
 mod ast;
 pub mod llvm;
 mod parser;
+
+mod compile;
 
 fn main() {
     let helloworld: ast::Action = "++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.".parse().unwrap();
@@ -11,4 +15,7 @@ fn main() {
     };
     // println!("{helloworld:#?}");
     Ctx::new(std::io::stdin(), std::io::stdout()).simulate(&helloworld);
+
+    let llvm = compile(&helloworld);
+    println!("{llvm}");
 }
